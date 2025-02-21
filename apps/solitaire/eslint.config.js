cd4ec0aa767fa773337bc0a020f852a4d2cca86f
mrth2 <mr.th2.90@gmail.com>
2025-02-21 11:55:58 +0700

    solitaire: initial & config eslint to forbid cross feature imports

--- apps/solitaire/eslint.config.js
+++ apps/solitaire/eslint.config.js
@@ -1,28 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact, { rules } from "eslint-plugin-react";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...rules,
+      "no-restricted-imports": ["error", {
+        zones: [
+          {
+            target: './src/features',
+            from: './src/app',
+          },
+          {
+            target: [
+              './src/components',
+              './src/hooks',
+              './src/utils',
+              './src/types',
+              './src/lib',
+            ],
+          }
+        ]
+      }],
     },
   },
-)
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
