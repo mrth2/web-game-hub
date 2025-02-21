38e72058797bdae3315c9217f0ab067b7990c754
mrth2 <mr.th2.90@gmail.com>
2025-02-21 22:30:05 +0700

    integrate GameProvider to app

--- apps/solitaire/src/app/app.tsx
+++ apps/solitaire/src/app/app.tsx
@@ -1,20 +1,24 @@
+import Foundation from "../features/foundation/components/Foundation"
 import Header from "../features/header/components/Header"
 import TableuBoard from "../features/tableau/components/TableuBoard"
+import GameProvider from "./gameContext"
 
-function App() {
+export default function App() {
   return (
-    <div className="bg-green-500 h-screen w-screen text-white flex flex-col">
-      <Header />
+    <GameProvider>
+      <div className="bg-green-500 h-screen w-screen text-white flex flex-col">
+        <Header />
 
-      <div className="flex justify-between flex-1">
-        <div className="w-20 bg-gray-700/50"></div>
-        <div className="flex-1">
-          <TableuBoard />
+        <div className="flex justify-between flex-1">
+          <div className="w-28 bg-gray-700/50"></div>
+          <div className="flex-1">
+            <TableuBoard />
+          </div>
+          <div className="w-28 bg-gray-700/50">
+            <Foundation />
+          </div>
         </div>
-        <div className="w-20 bg-gray-700/50"></div>
       </div>
-    </div>
+    </GameProvider>
   )
 }
-
-export default App
